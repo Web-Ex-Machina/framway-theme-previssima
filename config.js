5387f37a131cfd56ee70a946b2bd56489db9e150
SeptimusVII <vansteene.quentin@gmail.com>
2025-08-06 17:19:35 +0200

    set config to match previous assets

--- config.js
+++ config.js
@@ -1,1 +1,38 @@
-module.exports = {};
+module.exports = {
+	'spacing': '15px',
+	'radius': '5px',
+	'colors': {
+		'green':      '#5cb85c',
+		'orange':     '#DC6053',
+
+		'blue':       '#278ecc',
+		'darkblue':   '#0e81c6',
+		'darkerblue': '#06649A',
+		'yellow':     '#f3b40a',
+		'orangePro':  '#ff7200',
+
+		'lightgrey':  '#f0f0f0',
+		'greylight':  '#f0f0f0',
+		'grey':       '#6f6f6e',
+		'greySep':    '#d3d3d3',
+		'greystrong': '#707070',
+	},
+
+	'body':{
+		'background':       'colors(greylight)',
+		'block-background': 'colors(white)',
+		'font-color':       'colors(black)',
+	},
+	'input':{
+		'background':       'colors(white)',
+		'font-color':       'colors(grey)',
+		'border-color':       'colors(greySep)',
+	},
+	'input-focus':{
+		'background':       'colors(none)',
+		'border-color':       'colors(blue)',
+	},
+	'header':{
+		'font-color':       'colors(grey)',
+	},
+};
